c0e9045cc868894f521011f78d9a06f98bd7d2e1
jaronaearle <jaronaearle@gmail.com>
2023-09-10 00:15:41 -0600

    quick improvement for ls-tree

--- scripts/ls-tree.sh
+++ scripts/ls-tree.sh
@@ -2,18 +2,16 @@
 
 CMD="colorls --tree=1"
 
-if [ "$#" -eq 0 ]; then
-    CMD=$CMD
-else
+if [ ! "$#" -eq 0 ]; then
     for ARG in "$@"
     do
         if [[ $ARG  =~ ^[0-5]+$ ]]; then
-            TREE=$([ $ARG -lt 5 ] && echo $ARG || echo 1)
-            CMD=$(echo $CMD | sed -r "s/1/$TREE/g")
+            TREE=$([ "$ARG" -lt 5 ] && echo "$ARG" || echo 1)
+            CMD=$(echo "$CMD" | sed -r "s/1/$TREE/g")
         else
             CMD="${CMD} ${ARG}"
         fi
     done
 fi
 
-eval $CMD
+eval "$CMD"
