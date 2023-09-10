5af6dc18ade4dbdae1dd734928c6e3fea3e0c148
jaronaearle <jaronaearle@gmail.com>
2023-09-10 00:05:02 -0600

    ls tree depth wrapper script

---
+++ scripts/ls-tree.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CMD="colorls --tree=1"
+
+if [ "$#" -eq 0 ]; then
+    CMD=$CMD
+else
+    for ARG in "$@"
+    do
+        if [[ $ARG  =~ ^[0-5]+$ ]]; then
+            TREE=$([ $ARG -lt 5 ] && echo $ARG || echo 1)
+            CMD=$(echo $CMD | sed -r "s/1/$TREE/g")
+        else
+            CMD="${CMD} ${ARG}"
+        fi
+    done
+fi
+
+eval $CMD
